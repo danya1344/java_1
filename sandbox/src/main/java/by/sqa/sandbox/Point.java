5fdc7c50d3a0a0166e0ef10e89659c1cf934b4c4
danya1344 <91887458+danya1344@users.noreply.github.com>
2021-10-05 18:40:41 +0300

    connection TestNG

--- sandbox/src/main/java/by/sqa/sandbox/Point.java
+++ sandbox/src/main/java/by/sqa/sandbox/Point.java
@@ -3,10 +3,10 @@ package by.sqa.sandbox;
 public class Point {
     public static void main(String[] args) {
 
-        Pointdata P = new Pointdata(0, 1, 2, -2);
+        PointData P = new PointData(0, 1, 2, -2);
         System.out.println("points distance =" + distance(P));
     }
-    public static double distance (Pointdata P){
+    public static double distance (PointData P){
         return Math.sqrt((P.p3-P.p1)*(P.p3-P.p1)+(P.p4-P.p2)*(P.p4-P.p2));
     }
 }
