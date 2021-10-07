fec39e3c2cde1f08d46299b301f96f02d69d5af3
danya1344 <91887458+danya1344@users.noreply.github.com>
2021-10-07 09:49:47 +0300

    test addressbook v2.0

--- addressbook-web-tests/src/test/java/by/sqa/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/by/sqa/addressbook/GroupCreationTests.java
@@ -1,7 +1,7 @@
 package by.sqa.addressbook;
 
 import org.openqa.selenium.*;
-import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -10,28 +10,27 @@ import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.fail;
 
-public class UntitledTestCase {
+public class GroupCreationTests {
   private WebDriver driver;
   private boolean acceptNextAlert = true;
   private final StringBuffer verificationErrors = new StringBuffer();
 
   @BeforeClass(alwaysRun = true)
   public void setUp() {
-    driver = new OperaDriver();
+    System.setProperty("driver.gecko.driver","C:\\Users\\Lenovo\\java_1\\addressbook-web-tests\\geckodriver-v0.30.0-win32geckodriver.exe");
+    driver = new FirefoxDriver();
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
   }
 
   @Test
-  public void testUntitledTestCase() {
-    driver.get("http://localhost/addressbook/#");
-    driver.findElement(By.name("user")).click();
+  public void testGroupCreationTests() {
+    driver.get("http://localhost/addressbook/group.php");
     driver.findElement(By.name("user")).clear();
     driver.findElement(By.name("user")).sendKeys("admin");
     driver.findElement(By.name("pass")).click();
     driver.findElement(By.name("pass")).clear();
     driver.findElement(By.name("pass")).sendKeys("secret");
     driver.findElement(By.xpath("//input[@value='LOGIN']")).click();
-    driver.findElement(By.linkText("GROUPS")).click();
     driver.findElement(By.name("new")).click();
     driver.findElement(By.name("group_name")).click();
     driver.findElement(By.name("group_name")).clear();
@@ -44,13 +43,7 @@ public class UntitledTestCase {
     driver.findElement(By.name("group_footer")).sendKeys("test3");
     driver.findElement(By.name("submit")).click();
     driver.findElement(By.linkText("group page")).click();
-    driver.findElement(By.xpath("//form[@action='/addressbook/group.php']")).click();
-    driver.findElement(By.xpath("//form[@action='/addressbook/group.php']")).click();
     driver.findElement(By.linkText("LOGOUT")).click();
-    driver.findElement(By.name("user")).clear();
-    driver.findElement(By.name("user")).sendKeys("admin");
-    driver.findElement(By.name("pass")).clear();
-    driver.findElement(By.name("pass")).sendKeys("secret");
   }
 
   @AfterClass(alwaysRun = true)
