6931608b2bab228b6b7aeec92b63d2bd37788b2d
danya1344 <91887458+danya1344@users.noreply.github.com>
2021-10-13 13:24:47 +0300

    лекция 3.4

--- addressbook-web-tests/src/test/java/by/egar/addressbook/test/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/by/egar/addressbook/test/ContactDeletionTests.java
@@ -1,12 +1,14 @@
 package by.egar.addressbook.test;
 
 import by.egar.addressbook.model.ContactDatas;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ContactDeletionTests extends TestsBase {
 
   @Test
   public void testContactDeletion() throws Exception {
+    int before = app.getContactHelpers().getContactCount();
     app.getNavigationHelpers().gotoAdd_NewPage();
     if (! app.getContactHelpers().isThereContact()) {
       app.getContactHelpers().createContact(new ContactDatas("daniil", "astap", "daniil@qmail.ru", "test1"), true);
@@ -14,6 +16,8 @@ public class ContactDeletionTests extends TestsBase {
     app.getContactHelpers().selectContact();
     app.getContactHelpers().deleteSelectedContact();
     app.getNavigationHelpers().returnToHomePage();
+    int after = app.getContactHelpers().getContactCount();
+    Assert.assertEquals(after, before - 1);
   }
 }
 
--- addressbook-web-tests/src/test/java/by/egar/addressbook/test/ContactModTests.java
+++ addressbook-web-tests/src/test/java/by/egar/addressbook/test/ContactModTests.java
@@ -1,12 +1,14 @@
 package by.egar.addressbook.test;
 
 import by.egar.addressbook.model.ContactDatas;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ContactModTests extends TestsBase {
     @Test
     public void testContactMod() {
         //app.getNavigationHelpers().gotoAdd_NewPage();
+        int before = app.getContactHelpers().getContactCount();
         if (! app.getContactHelpers().isThereContact()) {
             app.getContactHelpers().createContact(new ContactDatas("daniil", "astap", "daniil@qmail.ru", "test1"), true);
         }
@@ -15,5 +17,7 @@ public class ContactModTests extends TestsBase {
         app.getContactHelpers().fillContactForm(new ContactDatas("daniil", null, null, null), false);
         app.getContactHelpers().submitContactMod();
         app.getNavigationHelpers().returnToHomePage();
+        int after = app.getContactHelpers().getContactCount();
+        Assert.assertEquals(after, before );
     }
 }
