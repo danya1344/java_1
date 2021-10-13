a751bce944c9b35f15cfa19713623505fb047268
danya1344 <91887458+danya1344@users.noreply.github.com>
2021-10-13 09:29:31 +0300

    лекция 3.3

--- addressbook-web-tests/src/test/java/by/egar/addressbook/appmanager/ApplicationContactManager.java
+++ addressbook-web-tests/src/test/java/by/egar/addressbook/appmanager/ApplicationContactManager.java
@@ -1,22 +1,33 @@
 package by.egar.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationContactManager {
 
-    private WebDriver wd;
+    WebDriver wd;
 
     private NavigationHelpers navigationHelpers;
     private ContactHelpers contactHelpers;
     private SessionHelpers sessionHelpers;
+    private String browser;
+
+    public ApplicationContactManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
         System.setProperty("webdriver.gecko.driver", "C:\\WebDrivers\\geckodriver.exe");
         System.setProperty("webdriver.chrome.driver", "C:\\WebDrivers\\chromedriver.exe");
-        wd = new FirefoxDriver();
+        if (browser == BrowserType.FIREFOX) {
+            wd = new FirefoxDriver();
+        } else if (browser == BrowserType.CHROME) {
+            wd = new ChromeDriver();
+        }
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
         contactHelpers = new ContactHelpers(wd);
--- addressbook-web-tests/src/test/java/by/egar/addressbook/test/TestsBase.java
+++ addressbook-web-tests/src/test/java/by/egar/addressbook/test/TestsBase.java
@@ -1,12 +1,13 @@
 package by.egar.addressbook.test;
 
 import by.egar.addressbook.appmanager.ApplicationContactManager;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class TestsBase {
 
-    protected final ApplicationContactManager app = new ApplicationContactManager();
+    protected final ApplicationContactManager app = new ApplicationContactManager(BrowserType.CHROME);
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
