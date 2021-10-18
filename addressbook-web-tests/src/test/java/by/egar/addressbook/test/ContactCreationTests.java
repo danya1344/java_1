fd007d652561e4b9e1215b911c54529185042e35
danya1344 <91887458+danya1344@users.noreply.github.com>
2021-10-18 10:45:08 +0300

    dz 15

--- addressbook-web-tests/src/test/java/by/egar/addressbook/test/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/by/egar/addressbook/test/ContactCreationTests.java
@@ -2,21 +2,35 @@ package by.egar.addressbook.test;
 
 import by.egar.addressbook.model.ContactDatas;
 import by.egar.addressbook.model.Contacts;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestsBase {
 
-    @Test
-    public void testContactCreation() {
+    @DataProvider
+    public Iterator<Object[]> validContacts() {
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[]{ new ContactDatas().withFirstname("firstname 1").withLastname("lastname 1").withAddress("address 1")});
+        list.add(new Object[]{new ContactDatas().withFirstname("firstname 2").withLastname("lastname 2").withAddress("address 2")});
+        list.add(new Object[]{new ContactDatas().withFirstname("firstname 3").withLastname("lastname 3").withAddress("address 3")});
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "validContacts")
+    public void testContactCreation(ContactDatas contact) {
+        File photo = new File("src/test/resourses/pngwing.png");
+        ContactDatas contact = new ContactDatas().withPhoto(photo);
         app.getGoTo().add_NewPage();
         Contacts before = app.contact().all();
-        File photo = new File("src/test/resourses/pngwing.png");
-        ContactDatas contact = new ContactDatas().withFirstname("daniil").withPhoto(photo);
         app.contact().create(contact);
         assertThat(app.contact().count(), equalTo(before.size() + 1));
         Contacts after = app.contact().all();
--- addressbook-web-tests/src/test/java/by/sqa/addressbook/test/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/by/sqa/addressbook/test/GroupCreationTests.java
@@ -2,19 +2,31 @@ package by.sqa.addressbook.test;
 
 import by.sqa.addressbook.model.GroupData;
 import by.sqa.addressbook.model.Groups;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
+    @DataProvider
+    public Iterator<Object[]> validGroups() {
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[]{ new GroupData().withName("test1").withHeader("header 1").withFooter("footer 1")});
+        list.add(new Object[]{"test2", "header 2", "footer 2"});
+        list.add(new Object[]{"test3", "header 3", "footer 3"});
+        return list.iterator();
+    }
 
-    @Test
-    public void testGroupCreation()  {
+    @Test(dataProvider = "validGroups")
+    public void testGroupCreation(GroupData group) {
         app.goTo().groupPage();
         Groups before = app.group().all();
-        GroupData group = new GroupData().withName("test2");
         app.group().create(group);
         assertThat(app.group().count(), equalTo(before.size() + 1));
         Groups after = app.group().all();
@@ -23,7 +35,7 @@ public class GroupCreationTests extends TestBase {
     }
 
     @Test
-    public void testBadGroupCreation()  {
+    public void testBadGroupCreation() {
         app.goTo().groupPage();
         Groups before = app.group().all();
         GroupData group = new GroupData().withName("test2");
